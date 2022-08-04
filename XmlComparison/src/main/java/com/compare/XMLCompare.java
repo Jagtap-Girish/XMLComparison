e5d640fe718d7cd83011c8f0a1188a9c3f462116
GJagtap <GJagtap@PRO-IND-1146.ad.prolifics.com>
2022-08-04 19:02:30 +0530

    latest cummit

--- XmlComparison/src/main/java/com/compare/XMLCompare.java
+++ XmlComparison/src/main/java/com/compare/XMLCompare.java
@@ -3,7 +3,6 @@ package com.compare;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,20 +10,19 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.apache.commons.io.FilenameUtils;
 import org.custommonkey.xmlunit.DetailedDiff;
 import org.custommonkey.xmlunit.Diff;
 import org.custommonkey.xmlunit.XMLUnit;
 import org.xml.sax.SAXException;
-import org.apache.commons.io.FilenameUtils;
 
 public class XMLCompare {
 	static ArrayList<String> output=new ArrayList<String>();
 	public static void main(String[] args) throws SAXException, IOException {
-		File dir1 = new File("/Users/gJagtap/OneDrive - Prolifics Corporation Ltd.,/Desktop/BAR1");
-		File dir2 = new File("/Users/gJagtap/OneDrive - Prolifics Corporation Ltd.,/Desktop/BAR2");
+		File dir1 = new File("/Users/gJagtap/OneDrive - Prolifics Corporation Ltd.,/Desktop/New Folder");
+		File dir2 = new File("/Users/gJagtap/OneDrive - Prolifics Corporation Ltd.,/Desktop/New Folder 1");
 		File[] files=dir1.listFiles();
 		File[] files1=dir2.listFiles();
-		
 		ArrayList<File> f=new ArrayList<File>();
 		ArrayList<File> f1=new ArrayList<File>();
 		for (int i = 0; i < files.length; i++) {
@@ -35,44 +33,35 @@ public class XMLCompare {
 		}
 		boolean flag=true;
 		for (Iterator<File> iterator = f1.iterator(); iterator.hasNext();) {
-			File file = (File) iterator.next();
-			//System.out.println(file.getName());
+			File file = iterator.next();
 			for (File file1 : f) {
-				if (file1.getName().equals(file.getName()) && file1.length()==file.length()) 
+				if (file1.getName().equals(file.getName()) && file1.length()==file.length())
 				{
 					flag=false;
 					output.add(file.getName());
 					String out=output.toString();
 					printOutput("commom file "+out);
-                  	System.out.println("These files are same in folders both ====> "+file.getName());
-				}else  
-					/*String ex=FilenameUtils.getExtension(file.getName());
-					String ex1=FilenameUtils.getExtension(file1.getName());*/
-					//System.out.println(ex+" "+ex1);
-					if (FilenameUtils.getExtension(file.getName()).equals("xml") && FilenameUtils.getExtension(file1.getName()).equals("xml"))
-					{
-						flag =false;
-							FileInputStream fileInputStream= new
-								FileInputStream(file);
-						FileInputStream fileInputStream1= new
-								FileInputStream(file1);
-						BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(fileInputStream));
-						BufferedReader bufferedReader1 =new BufferedReader(new InputStreamReader(fileInputStream1));
-						XMLUnit.setIgnoreWhitespace(true);
-						List<?> differences=comapare(bufferedReader,bufferedReader1);
-						printDifference(differences);
-					
+					System.out.println("These files are same in folders both ====> "+file.getName());
 				}
-					
+				if (FilenameUtils.getExtension(file.getName()).equals("xml") && FilenameUtils.getExtension(file1.getName()).equals("xml"))
+				{
+					flag =false;
+					FileInputStream fileInputStream= new
+							FileInputStream(file);
+					FileInputStream fileInputStream1= new
+							FileInputStream(file1);
+					BufferedReader bufferedReader =new BufferedReader(new InputStreamReader(fileInputStream));
+					BufferedReader bufferedReader1 =new BufferedReader(new InputStreamReader(fileInputStream1));
+					XMLUnit.setIgnoreWhitespace(true);
+					List<?> differences=comapare(bufferedReader,bufferedReader1);
+					printDifference(differences);
+				}
 			}
 		}
 		if(flag) {
 			System.out.println("All files are different");
 		}
 	}
-
-
-
 	private static List<?> comapare(BufferedReader bufferedReader, BufferedReader bufferedReader1) throws SAXException, IOException {
 		Diff diferences = new Diff(bufferedReader, bufferedReader1);
 		DetailedDiff detailedDiff=new DetailedDiff(diferences);
@@ -86,19 +75,12 @@ public class XMLCompare {
 		for (Object difference : differences) {
 			System.out.println(difference);
 			String out=difference.toString();
-			
 			printOutput(out);
 		}
 	}
-
-
-
 	private static void printOutput(String difference) throws IOException {
-		// TODO Auto-generated method stub
 		FileOutputStream output=new FileOutputStream("/Users/gJagtap/OneDrive - Prolifics Corporation Ltd.,/Desktop/Output.txt");
-	
 		byte[] getBytes=difference.getBytes();
 		output.write(getBytes);
 	}
-
 }
